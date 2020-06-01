eca950a3ffbae5244f0fdc49e879a068778bcda5
oguzzerman <oguz.zerman@gmail.com>
2020-06-01 12:10:02 +0300

    Add solution

---
+++ GLibrary/GLHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLibrary
+{
+    public static class GLHelper
+    {
+
+        public static float[] LoadTexture(string fileName, out int width, out int height)
+        {
+            float[] r;
+            using (var bmp = ConvertToBitmap(fileName))
+            {
+                width = bmp.Width;
+                height = bmp.Height;
+                r = new float[width * height * 4];
+                int index = 0;
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        var pixel = bmp.GetPixel(x, y);
+                        r[index++] = pixel.R / 255f;
+                        r[index++] = pixel.G / 255f;
+                        r[index++] = pixel.B / 255f;
+                        r[index++] = pixel.A / 255f;
+                    }
+                }
+            }
+            return r;
+        }
+
+        public static BitmapData LoadImage(string path)
+        {
+            Bitmap bmp = new Bitmap(ConvertToBitmap(path));
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+
+            BitmapData ret = bmp.LockBits(rect, ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            bmp.UnlockBits(ret);
+
+            return ret;
+        }
+
+        public static Bitmap ConvertToBitmap(string fileName)
+        {
+            Bitmap bitmap;
+            using (Stream bmpStream = System.IO.File.Open(fileName, System.IO.FileMode.Open))
+            {
+                Image image = Image.FromStream(bmpStream);
+
+                bitmap = new Bitmap(image);
+
+            }
+            return bitmap;
+        }
+
+    }
+}
---
+++ GLibrary/TextureData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLibrary
+{
+    public class TextureData
+    {
+        #region Ctor
+
+        public TextureData()
+        {
+
+        }
+        public TextureData(string fileName)
+        {
+            _Data =  GLHelper.LoadTexture(fileName, out _Width, out _Height);
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        private float[] _Data;
+        private int _Width;
+        private int _Height;
+
+        #endregion
+
+        #region Private Properties
+
+        public float[] Data { get => _Data; set => _Data = value; }
+        public int Width { get => _Width; set => _Width = value; }
+        public int Height { get => _Height; set => _Height = value; }
+
+        #endregion
+
+    }
+}
